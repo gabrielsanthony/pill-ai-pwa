fb6983ce9b351b873d147178e9c23e4837cd6f22
gabrielsanthony <generation.gabriel@gmail.com>
2025-09-08 00:51:08 +0000

    Cheer Squad: inviteCodes (CORS+region), rules wired, UI hookups

--- functions-v2/index.js
+++ functions-v2/index.js
@@ -1,3 +1,5 @@
 // functions-v2/index.js
-export { sendReminders } from './sendReminders.js';
 export { createInviteCode, redeemInviteCode, removeCheerLink } from './inviteCodes.js';
+
+// Re-enable later if/when you add the file:
+// export { sendReminders } from './sendReminders.js';
